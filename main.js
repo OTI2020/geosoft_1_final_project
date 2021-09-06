9109dc24d29e46c3f01fe9205ff47b5f5cef3e65
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-06 22:41:16 +0200

    trying geojson with leaflet

--- main.js
+++ main.js
@@ -1,191 +1,105 @@
-
-
-main()
-/**
- * main function is needed befor interacting of users could be possible
- * creates leaflet-map, GEO1-marker, loads route_4.geojson and toolbar
- */
-function main() {
-    var start_map = create_map_with_route_and_marker()
-    get_toolbar_functionality(start_map)
-}
+var map = L.map('map'
+                //,{minZoom: 10}
+               ).setView([51.975, 7.61], 13);
 
 
 
 
-/**
- * @returns map - leaflat map, that contains tilelayer, markerGEO1 and 
- * given geoJSON route from the learnweb
- */
-function create_map_with_route_and_marker() {
-    var map=L.map('map').setView([51, 9], 13)
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 18,
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
-        id: "osm"
-    }).addTo(map)
-
-    var markerGEO1 = L.marker([51.969, 7.596])
-    markerGEO1.addTo(map)
-    markerGEO1.bindPopup("<b> Hello Tutor! <br /> GEO1 is near").openPopup()
-    // console.log("markerGEO1")
-    // console.log(markerGEO1)
-
-    var myRoute = L.geoJSON(routeVar)
-    myRoute.addTo(map)
-    // console.log("myRoute")
-    // console.log(myRoute)
-
-    return map
-}
+// add an OpenStreetMap tile layer
+var osm = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 
 
 
-/**
- * here draw plugin beginns; Control.Drawn, Event ...
- * Makes rectangle draw tool invisible after useing one time.
- * @function get_intersection_points()
- * @function create_weather_popups()
- * @param {leaflet-map} in_map 
- */
-function get_toolbar_functionality(in_map) {
-    var drawnItems = L.featureGroup().addTo(in_map)
-    
-    // activate only needed functionality
-    in_map.addControl(new L.Control.Draw( {
-        edit: {
-            featureGroup: drawnItems,
-            poly: {
-                allowIntersection: false
-            }
+// add the humanitarian OpenStreetMap layer
+var hotOSM = new L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team'
+}).addTo(map);
+
+
+
+
+// add a marker in the given location, attach some popup content to it and open the popup
+var ifgi_marker = L.marker([51.969279, 7.595796]).addTo(map);
+ifgi_marker.bindPopup('ifgi');
+ifgi_marker.openPopup();
+
+
+
+
+// add circle to 
+var ms_city_circle = new L.circle([51.961693, 7.627931], 250, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+ms_city_circle.bindPopup("Münster Innenstadt. <br/><img src='https://www.uni-muenster.de/imperia/md/images/zentral/international/bildergalerie/prinzipalmarkt_panorama_1200px.jpg'/ width='300px'>");
+ms_city_circle.openPopup();
+
+
+
+
+var church_Ueberwasserkirche_GeojsonFeature = {
+    "type": "Feature",
+        "properties": {
+          "name": "Ueberwasserkirche",
+          "URL": "https://en.wikipedia.org/wiki/%C3%9Cberwasserkirche",
+          "description": "schoene Kirche"
         },
-        draw: {
-            polyline: true,
-            polygon: false,
-            marker: true,
-            circle:false,
-            rectangle: false
-        }
-    }))
-
-    
-    in_map.on(L.Draw.Event.CREATED, function (e) {
-        // var type = e.layerType
-        var layer = e.layer
-        // add this layer to drawnItems
-        drawnItems.addLayer(layer)
-        
-        // to prevent that more than one polygone gets created
-        // this toolbar functionality gets hidden
-        $(".leaflet-draw-toolbar-top").css("visibility", "hidden")
-
-        // only als GeoJSON drawnItems can be used in the following
-        var polygon_for_intersection = drawnItems.toGeoJSON()
-
-        // drawnItems represents the drawn rectangle that gets 
-        // intersected with the given route in the following function
-        var out_points = get_intersection_points(routeVar, polygon_for_intersection)
-
-        // variable out_points represents intersection points as geoJson
-        create_weather_popups(in_map, out_points)
-    })
-
-    in_map.on('draw:edited', function (e) {
-        var layers = e.layers
-        layers.eachLayer(function (layer) {
-        })
-    })
-}   
-    
-
-
-
-
-/**
- * called by get_toolbar_functionality in case of an drawn.event
- * @param {PolyString/Polygon} in_line_1 
- * @param {PolyString/Polygon} in_line_2 
- * @returns intersects - all intersection points between the given 
- * geojson-route and the drawn rectangle
- * using turf for this (see References on html-page)
- */
-function get_intersection_points(in_line_1, in_line_2) {
-    var intersects = turf.lineIntersect(in_line_1, in_line_2)
-    return intersects
-}
-
-
-
-
-/**
- * called by get_toolbar_functionality in case of an drawn.event
- * calls @function get_weather_data() to fill popup with content
- * @param {leaflet-map} in_map 
- * @param {geoJSON} in_points - all intersection points - return of @function get_intersection_points()
- */
-function create_weather_popups(in_map, in_points) {
-    // this number/length is needed  for the following for-loop
-    var number_of_insections = in_points.features.length
-    
-    // for each intersection point a marker and a popup with 
-    // weather information gets created 
-    for (let i=0; i<number_of_insections; i++) {
-        var weather_marker = L.marker([in_points.features[i].geometry.coordinates[1], in_points.features[i].geometry.coordinates[0]])
-        weather_marker.addTo(in_map)
-
-        // individual name for every popup/marker
-        var m_name = 'marker_' + (i+1)
-        console.log(m_name);
-
-        // get data of current weather
-        // bind popup and so on in following funktion, 
-        // because of asynchronity, otherwise the object/string gets undefined
-        get_weather_data(in_points.features[i].geometry.coordinates[1], in_points.features[i].geometry.coordinates[0], weather_marker, m_name)
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                7.622365951538085,
+                51.964266412705875
+              ],
+              [
+                7.622333765029907,
+                51.96399537718307
+              ],
+              [
+                7.6233744621276855,
+                51.964028430383344
+              ],
+              [
+                7.623385190963744,
+                51.96420030663192
+              ],
+              [
+                7.623267173767089,
+                51.96431268689964
+              ],
+              [
+                7.62248396873474,
+                51.964266412705875
+              ],
+              [
+                7.622365951538085,
+                51.964266412705875
+              ]
+            ]
+        ]
     }
 }
 
+var overlay = L.geoJson(church_Ueberwasserkirche_GeojsonFeature);
+overlay.bindPopup(church_Ueberwasserkirche_GeojsonFeature.properties.name);
 
 
 
-/**
- * function requests weather information and put this in popups
- * @param {*} in_lat - latitude of individual intersectionpoint
- * @param {*} in_long - longitude of individual intersectionpoint
- * @param {*} in_weather_marker - marker variable that gets filled and a popup bind here
- * @param {*} in_m_name - individual names of markers of intersection points 
- * ajax syntax from: https://stackoverflow.com/questions/49662431/display-openweathermap-api-data-on-website-using-ajax
- */
-function get_weather_data(in_lat, in_long, in_weather_marker, in_m_name) {
-    // pleace past Your API key here:
-    const in_API_key = '***********************'
-    var resourse_url = "https://api.openweathermap.org/data/2.5/weather?lat="+in_lat+"&lon="+in_long+"&appid="+in_API_key
 
-    $.ajax({
-        url: resourse_url,
-        type: "GET",
-        dataType: "jsonp",
-        success: function(data){
-            
-            /*
-            console.log(data)
-            console.log(data.weather[0].main)
-            console.log(data.main)
-            console.log(data.main.temp)
-            */
+//add layer switcher
+var baseMaps = {
+    "OSM": osm,
+    "humanitarian OSM": hotOSM
+};
 
-            // rounded (2 decimal places) coordinates for the popup
-            var x_coor = Math.round(data.coord.lon*100)/100
-            var y_coor = Math.round(data.coord.lat*100)/100
-        
-            // variable weather_data contains all information that will be seen in the popup
-            var weather_data = '<b> This is ' + in_m_name + '</b> <br /> coordinates(lon,lat): (' + x_coor + ','+ y_coor + ') <br /> sea level: ' + data.main.sea_level + '<br /> current Weather: ' + data.weather[0].description + '<br /> temperature: ' + data.main.temp + ' F'
-            
-            // fill popup with information
-            in_weather_marker.bindPopup(weather_data).openPopup()
-        }
-    })
-}
+var overlayMaps = {
+    "nice_church": overlay
+};
 
-
+L.control.layers(baseMaps, overlayMaps).addTo(map);
