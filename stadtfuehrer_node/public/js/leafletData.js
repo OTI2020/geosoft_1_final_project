dafee3871cf03a0d88f5a3f5f8573e1a726a94f7
TobiasBrand-GI <tobias.brand1@web.de>
2021-09-15 21:34:32 +0200

    added Wikipedia API

--- stadtfuehrer_node/public/js/leafletData.js
+++ stadtfuehrer_node/public/js/leafletData.js
@@ -1,44 +1,7 @@
 function fillPopupHTML(name, url, json, exist){
-    let Lname=name;
-    let Lurl=url;
-    let Ljson=json;
-    let pathstr;
-    let btntxt;
-    let describehtml;
-    let describetxt;
-    let delbtn;
-    if(exist==0){
-        pathstr="/add/newpoi";
-        btntxt="Speichern";
-        describehtml="";
-        delbtn="";
-    }else if(exist==1){
-        pathstr="/update/poi";
-        btntxt="Aktualisieren";
-        describetxt=getWikipediaData(url);
-        describehtml="<div class='form-group odd'><label>Beschreibung:&nbsp;</label><label name='pdescribe'>"+describetxt+"</label></div>"
-        delbtn="&nbsp;&nbsp;<button class='btn btn-danger' type='submit' name='action' value='delete'>Löschen</button>";
-    }
-    let htmlString="<form action='"+pathstr+"' method='post' class='form-horizontal' role='form'>"+
-        "<fieldset>"+
-            "<div class='form-group odd'>"+
-                "<label class='form-label'>Name:&nbsp;</label>"+
-                "<input type='text' class='form-input' name='pname' value='"+Lname+"'/>"+
-            "</div>"+
-            "<div class='form-group even'>"+
-                "<label>URL (Wikipedia):&nbsp;</label>"+
-                "<input type='text' name='purl' value='"+Lurl+"'/>"+
-            "</div>"+
-            describehtml+
-            "<div class='form-group even'>"+
-                "<label>Daten:&nbsp;</label>"+
-                "<input type='text' name='pjson' readonly='readonly' value='"+Ljson+"'/>"+
-            "</div>"+
-        "</fieldset>"+
-        "<button class='btn btn-primary' type='submit' name='action' value='update'>"+btntxt+"</button>"+delbtn+
-    "</form>";
-    return htmlString;
+    return getWikipediaData(name,url,json,exist);
 }
+   
 function getDatafromDB() { 
     {$.ajax({ //handle request via ajax
         url: "/search", //request url is the prebuild request
@@ -66,3 +29,45 @@ function getDatafromDB() {
         })
     }
 } 
+function generateHTML(name,url,json,exist, description){
+    let Lname=name;
+    let Lurl=url;
+    let Ljson=json;
+    let pathstr;
+    let btntxt;
+    let describehtml;
+    let describetxt;
+    let delbtn;
+    if(exist==0){
+        pathstr="/add/newpoi";
+        btntxt="Speichern";
+        describehtml="";
+        delbtn="";
+    }else if(exist==1){
+        pathstr="/update/poi";
+        btntxt="Aktualisieren";
+        describetxt=description;
+        describehtml="<div class='form-group odd'><label>Beschreibung:&nbsp;</label><label name='pdescribe'>"+describetxt+"</label></div>"
+        delbtn="&nbsp;&nbsp;<button class='btn btn-danger' type='submit' name='action' value='delete'>Löschen</button>";
+    }
+
+    let htmlString="<form action='"+pathstr+"' method='post' class='form-horizontal' role='form'>"+
+        "<fieldset>"+
+            "<div class='form-group odd'>"+
+                "<label class='form-label'>Name:&nbsp;</label>"+
+                "<input type='text' class='form-input' name='pname' value='"+Lname+"'/>"+
+            "</div>"+
+            "<div class='form-group even'>"+
+                "<label>URL (Wikipedia):&nbsp;</label>"+
+                "<input type='text' name='purl' value='"+Lurl+"'/>"+
+            "</div>"+
+            describehtml+
+            "<div class='form-group even'>"+
+                "<input type='hidden' name='pjson' readonly='readonly' value='"+Ljson+"'/>"+
+            "</div>"+
+        "</fieldset>"+
+        "<button class='btn btn-primary' type='submit' name='action' value='update'>"+btntxt+"</button>"+delbtn+
+    "</form>";
+
+    return htmlString;
+}
--- stadtfuehrer_node/public/js/wikipedia-data.js
+++ stadtfuehrer_node/public/js/wikipedia-data.js
@@ -1,3 +1,68 @@
-function getWikipediaData(url){
-    
+function getWikipediaData(name,url,json,exist){
+    if(exist==1){
+    let urlslashsplit=url.split("/");
+    let urldotsplit=urlslashsplit[2].split(".")
+    let lang=urldotsplit[0];
+    let title=urlslashsplit[4];
+    if(checkURL(url, urldotsplit)==true){
+        console.log(title)
+        // http://www.zacwitte.com/getting-wikipedia-summary-from-the-page-id
+            $.ajax({
+              url: 'http://' + lang + '.wikipedia.org/w/api.php',
+              data: {
+                action:'query',
+                titles:title,
+                prop:"extracts",
+                format:'json'
+              },
+              dataType:'jsonp',
+              
+              success: function(data) {
+                  console.log(data);
+                $.ajax({
+                  url: 'http://' + lang + '.wikipedia.org/w/api.php',
+                  data: {
+                    action: 'parse',
+                    prop: "text",
+                    pages: title,
+                    format:'json'
+                  },
+                  dataType:'jsonp',
+                  success: function(data) {
+                      console.log(data)
+                      return generateHTML(name,url,json,exist,data );
+                    /*wikipage = $("<div>"+data.parse.title['*']+"<div>").children('p:first');
+                    wikipage.find('sup').remove();
+                    wikipage.find('a').each(function() {
+                      $(this)
+                        .attr('href', 'http://' + lang + '.wikipedia.org'+$(this).attr('href'));
+                        
+                    });*/
+                    
+                  }
+                  
+                });
+                
+              }
+            });
+            
+    }else {
+        return generateHTML(name,url,json,exist,"Keine Informationen verfügbar.")
+    }}else return generateHTML(name,url,json,exist,"");
+}
+function checkURL(lurl, splitarray){
+    console.log("Checks url")
+
+    try {
+        const myURL = new URL(lurl);
+        console.log(lurl);
+        if(splitarray[1]==="wikipedia" && splitarray[2]==="org"){
+            return true;
+        }else return false;
+        
+    } catch (error) {
+        console.log(`${Date().toString()}: ${error.input} is not a valid url`);
+        console.log( res.status(400).send(`${error.input} is not a valid url`));
+        return false;
+    }
 }
