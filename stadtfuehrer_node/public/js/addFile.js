6a3477fe3aedb1d0c87db469ef4e97e09aeb9a6e
TobiasBrand-GI <tobias.brand1@web.de>
2021-09-16 20:28:15 +0200

    file upload now works

--- stadtfuehrer_node/public/js/addFile.js
+++ stadtfuehrer_node/public/js/addFile.js
@@ -1,29 +1,35 @@
-function dateiupload(evt) {
-  console.log("test")
-    var dateien = evt.target.files; // FileList objekt
-
-    // erste Datei auswählen (wichtig, weil IMMER ein FileList Objekt generiert wird)
-    var uploadDatei = dateien[0];
-
-    // Ein Objekt um Dateien einzulesen
-    var reader = new FileReader();
-
-    var senddata = new Object();
-    // Auslesen der Datei-Metadaten
-    senddata.name = uploadDatei.name;
-    senddata.date = uploadDatei.lastModified;
-    senddata.size = uploadDatei.size;
-    senddata.type = uploadDatei.type;
-
-    // Wenn der Dateiinhalt ausgelesen wurde...
-    reader.onload = function(theFileData) {
-      senddata.fileData = theFileData.target.result; // Ergebnis vom FileReader auslesen
-
-      /*
-      Code für AJAX-Request hier einfügen
-      */
-    }
-
-    // Die Datei einlesen und in eine Data-URL konvertieren
-    reader.readAsDataURL(uploadDatei);
+document.querySelector("#read-button").addEventListener('click', function() {
+  if(document.querySelector("#file-input").files.length == 0) {
+    alert('Error : No file selected');
+    return;
   }
+  // file selected by user
+  let file = document.querySelector("#file-input").files[0];
+
+  // file MIME type
+  let filename = (file.name).split(".");
+  let file_type = filename[1];
+  console.log(file_type)
+  if(file_type=="json" || file_type=="geojson"){
+  	// new FileReader object
+    let reader = new FileReader();
+
+    // event fired when file reading finished
+    reader.addEventListener('load', function(e) {
+      // contents of the file
+        let text = e.target.result;
+        document.querySelector("#jsonText").textContent = text;
+        document.getElementById("jsonText").value=text;
+    });
+
+    // event fired when file reading failed
+    reader.addEventListener('error', function() {
+        alert('Error : Failed to read file');
+    });
+
+    // read file as text file
+    reader.readAsText(file);
+  }else{
+    alert('Error : Wrong File format selected');
+  }
+});
