8138bb6f2e68665aeb7d888f18cbe1bbf1385e81
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-18 20:51:53 +0200

    trying to connect with mongoDB and fix syntax

--- stadtfuehrer_node/public/js/addFile.js
+++ stadtfuehrer_node/public/js/addFile.js
@@ -39,56 +39,79 @@ if (document.querySelector("#poi-read-button")) {
   });
   
 
-
-
 /////////////////////////////////////////////////////////////////////
 
 } else if (document.querySelector("#tour-read-button")) {
   console.log("tour-raed-button was used");
 
-/////////////////////////////////////////////////////////////////////
-// similar process in the following, but for tours and not for pois
-/////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////
+  //
+  // similar process in the following, but for tours and not for pois
+  //
+  /////////////////////////////////////////////////////////////////////
 
 
 
+  document.querySelector("#tour-read-button").addEventListener('click', function() {
+    if(document.querySelector("#tour-file-input").files.length == 0) {
+      alert('Error : No file selected');
+      return;
+    }
+    // file selected by user
+    let file = document.querySelector("#tour-file-input").files[0];
+  
+    // file MIME type
+    let filename = (file.name).split(".");
+    let file_type = filename[1];
+    console.log(file_type)
+    if(file_type=="json" || file_type=="geojson"){
+      // new FileReader object
+      let reader = new FileReader();
+  
+      // event fired when file reading finished
+      reader.addEventListener('load', function(e) {
+  
+        // contents of the file
+          var tour_json_text = e.target.result;
+          document.querySelector("#tour-jsonText").textContent = tour_json_text;
+          document.getElementById("tour-jsonText").value=tour_json_text;
+  
+          console.log("text");
+          console.log(tour_json_text);
+          console.log("tour-jsonText");
+          console.log(document.getElementById("tour-jsonText"));
+      });
+  
+      // event fired when file reading failed
+      reader.addEventListener('error', function() {
+          alert('Error : Failed to read file');
+      });
+  
+      // read file as text file
+      reader.readAsText(file);
+    }else{
+      alert('Error : Wrong File format selected');
+    }
+  });
 
-document.querySelector("#tour-read-button").addEventListener('click', function() {
-  if(document.querySelector("#tour-file-input").files.length == 0) {
-    alert('Error : No file selected');
-    return;
-  }
-  // file selected by user
-  let file = document.querySelector("#tour-file-input").files[0];
 
-  // file MIME type
-  let filename = (file.name).split(".");
-  let file_type = filename[1];
-  console.log(file_type)
-  if(file_type=="json" || file_type=="geojson"){
-  	// new FileReader object
-    let reader = new FileReader();
 
-    // event fired when file reading finished
-    reader.addEventListener('load', function(e) {
+  // save json in mongoDB Collection for tours
 
-      // contents of the file
-        let text = e.target.result;
-        document.querySelector("#tour-jsonText").textContent = text;
-        document.getElementById("tour-jsonText").value=text;
-    });
+  var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 
-    // event fired when file reading failed
-    reader.addEventListener('error', function() {
-        alert('Error : Failed to read file');
-    });
 
-    // read file as text file
-    reader.readAsText(file);
-  }else{
-    alert('Error : Wrong File format selected');
-  }
-});
+  MongoClient.connect('mongodb://127.0.0.1:27017/tours', function(err,db) {
+    if (err) throw err;
+    console.log("Connected to Database");
+
+    // insert record
+    db.collection('tours').insert(tour_json_text, function(err, records) {
+        if (err) throw err;
+        console.log("Record added as " + records[0]._id);
+      });
+  });
+
 
 
 // 2. else-case: no button
