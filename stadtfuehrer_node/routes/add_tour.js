eefcb20e02bd37247385710fb9848cb592815ea4
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-20 01:30:33 +0200

    prepair adding tours manuel on tours_config.html

--- stadtfuehrer_node/routes/add_tour.js
+++ stadtfuehrer_node/routes/add_tour.js
@@ -15,6 +15,7 @@ const collectionName = 'tours' // collection name
 // const gjv = require("geojson-validation");
 
 router.post('/newtour', function(req, res, next) {
+  // JSON.stringify(req.body)
   console.log("A new tour has been added")
   let tour = {}
   tour.json = req.body.t_json
@@ -50,3 +51,43 @@ router.post('/newtour', function(req, res, next) {
 });
     
 module.exports = router;
+
+
+
+////////////////////////////////////////////////////////
+// adding tour on tours_config.html
+
+
+router.post('/newTourManuel', function(req, res, next) {
+  console.log("A new tour has been added")
+  
+  //Create Payload to Store
+  var tourName = req.body.tourName;
+  var trimmedPois = req.body.poisNames.substring(0, req.body.poisNames.length);
+  var pois = trimmedPois.split(',');
+  console.log("Test3")
+
+  //catching empty input
+  if(tourName.json==""){
+    console.log("empty JSON");
+    res.redirect("/errorData_tour.html");
+    return false;
+  }
+  // connect to the mongodb database and afterwards, insert one the new element
+  client.connect(function(err){
+    assert.equal(null, err)
+    console.log('Connected successfully to server')
+    const db = client.db(dbName)
+    const collection = db.collection(collectionName)
+    
+    // Insert the document in the database
+    collection.insertOne(tour, function(err, result) {
+      assert.equal(err, null)
+      assert.equal(1, result.result.ok)
+      // console.log(result)
+      console.log(`Inserted ${result.insertedCount} document into the collection`)
+      res.redirect('/tours_config.html')
+    })
+  })
+});
+module.exports = router; //export as router
