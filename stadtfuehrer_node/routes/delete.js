f3b31d84a77c34b4b8da51c9d0ba76d4a83cf7b8
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-20 02:45:06 +0200

    prepair posibillity to delete tours - I've no idea if it works :/

--- stadtfuehrer_node/routes/delete.js
+++ stadtfuehrer_node/routes/delete.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded( {extended: true} )); // because Error: 'body-parser 
 const url = 'mongodb://localhost:27017' // connection URL
 const dbName = 'stadtfuehrerDB' // database name
 const collectionName = 'pois' // collection name
+const toursCollectionName = 'tours' // other collection name
 //----------------------------------------------------------------------------------------------------------------------
 const MongoClient = require('mongodb').MongoClient
 const client = new MongoClient(url) // mongodb client
@@ -46,6 +47,40 @@ router.post('/removeEntry', function(req, res, next)
                 res.redirect('/index.html')
             }
 
+        })
+
+    })
+})
+module.exports = router; //export as router
+
+
+
+
+///////////////////////////////////////////////////////////
+// delete Tours
+
+
+//Delete Tour - this post operation can be used to remove existing tours from the tours collection 
+router.post('/removeTour', function(req, res, next)
+{
+    client.connect(function(err)
+    {
+        console.log('Connected successfully to server')
+        const db = client.db(dbName)
+        const collection = db.collection(toursCollectionName)
+        var TourNameToDelete = req.body.TourNameToDelete;
+        
+        collection.find({TourNameToDelete: TourNameToDelete}).toArray(function(err, docs)
+        {      
+            if(docs.length >= 1){ //check if tour exists
+                collection.deleteOne({TourNameToDelete: TourNameToDelete}, function(err, results){ //delte the tour from the tours collection
+                })
+                res.sendFile(__dirname + "/tours_config.html") //send positive response -> the post operation war successful
+            }
+            else { //if the tour does not exist
+                res.sendFile(__dirname + "/errorData_tour.html") //send nonexistent tour error
+            }
+            
         })
 
     })
