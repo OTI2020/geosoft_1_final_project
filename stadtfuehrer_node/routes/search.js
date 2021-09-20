33cca3855d9a176edecdb6da9eac83ec6c0cf053
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-20 12:05:03 +0200

    successful consolen log of tourCollection Documents :)

--- stadtfuehrer_node/routes/search.js
+++ stadtfuehrer_node/routes/search.js
@@ -7,10 +7,11 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017' // connection URL
 const dbName = 'stadtfuehrerDB' // database name
 const poisCollectionName = 'pois' // collection name
-const toursCollectionName = 'tours' // collection name
+//const toursCollectionName = 'tours' // collection name
 //----------------------------------------------------------------------------------------------------------------------
 const MongoClient = require('mongodb').MongoClient //Client for MongoDB
 const client = new MongoClient(url) // mongodb client
+
 
 //get Documents
 router.get('/', function(req, res, next) 
@@ -21,7 +22,7 @@ router.get('/', function(req, res, next)
     assert.strictEqual(null, err);
   
     const db = client.db(dbName); //Database
-    const collection = db.collection(collectionName); //Collection
+    const collection = db.collection(poisCollectionName); //Collection
 
     // Find all documents
     var result = [];
@@ -34,37 +35,3 @@ router.get('/', function(req, res, next)
 });
 module.exports = router; //export as router
 
-
-
-
-///////////////////////////////////////////////////////////
-// get All Tours and their pois
-
-router.get('/getCollections', function(req, res, next) 
-{
-  //Connect to the mongodb database and retrieve all docs
-  client.connect(function(err) 
-  {
-    const db = client.db(dbName); //Database
-    const t_collection = db.collection(toursCollectionName); //tours collection
-    const p_collection = db.collection(poisCollectionName); //pois collection
-
-    // Find all documents
-    var t_result = []; //tour result
-    var p_result = []; //location result
-    t_collection.find({}).toArray(function(err, docs_1) 
-    {
-      t_result = docs_1; //store tours
-
-      p_collection.find({}).toArray(function(err, docs_2) 
-      {
-        p_result = docs_2; //store pois
-        var result = [];
-        result[0] = p_result;
-        result[1] = t_result; 
-        res.json(result); //return combines result
-      })
-    })
-  })
-});
-module.exports = router; //export as router
