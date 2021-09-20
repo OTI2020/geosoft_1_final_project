6e5c0eee71738b4dc0abd47804ce35255d09c6fb
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-20 14:34:47 +0200

    add console.logs to find mistakes and delete wrong comments

--- stadtfuehrer_node/public/js/manage_dbCollection_interacting.js
+++ stadtfuehrer_node/public/js/manage_dbCollection_interacting.js
@@ -1,36 +1,15 @@
 
-var pois_array
 var tours_array
 /*
-* @function getAllfromDB - retrieve all data (locations and tours) from mongoDB
+* @function getDatafromDB - retrieve all tours from mongoDB tour-Collection
 */
-/*
-function getAllfromDB() { 
-   {$.ajax({ //handle request via ajax
-       url: "/search/getCollections", //request url is the prebuild request
-       method: "GET", //method is GET since we want to get data not post or update it
-       async: false
-       })
-       .done(function(res) { //if the request is done -> successful
-           pois_array = res[0]; //store locations in locations array
-           tours_array = res[1]; //store tours in tours_array
-       },
-       )}
-}
-getAllfromDB();
-*/
-
-var tour_response;
 function getDatafromDB() { 
     {$.ajax({ //handle request via ajax
         url: "/search_tour/getCollection", //request url is the prebuild request
         method: "GET", //method is GET since we want to get data not post or update it
         })
         .done(function(res) { //if the request is done -> successful
-            //bind a popupto the given marker / the popupt is formatted in HTML and 
-            //is enriched with information extracted from the api response
-            //console.log(response[0].geojson.features[0]);
-            tour_response = res;
+            tours_array = res; //store tours in tours_array
         })
         .fail(function(xhr, status, errorThrown) { //if the request fails (for some reason)
             console.log("Request has failed!", '/n', "Status: " + status, '/n', "Error: " + errorThrown); //we log a message on the console
@@ -41,12 +20,19 @@ function getDatafromDB() {
     }
 }   
 getDatafromDB()
+console.log("tours_array test_1");
+console.log(tours_array);
+
 
 
 /**
  * @function selectTourForDelete - function adds a tour to be deleted to the the deleteTour form
  */
  function selectTourForDelete() {
+     console.log("tours_array test_2");
+     console.log(tours_array);
+
+
     var value = document.getElementById("selectTourToDelete").value; //get value from toggler
     for(var i = 0; i < tours_array.length; i++) { //iterate over forms 
         if(tours_array[i].tourName == value) { //if criteria are met
