e18512b9f7b3ae4747ad763717210cb24eb19d11
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-09-20 02:47:43 +0200

    add search for Collections to make getAllfromDB() from manage_dbCollection_interacting.js useable

--- stadtfuehrer_node/routes/search.js
+++ stadtfuehrer_node/routes/search.js
@@ -6,7 +6,8 @@ const assert = require('assert');
 //-------------->>>>Hier muss die passende Datenbank und die passende Collection angegeben werden!!!!!<<<<--------------
 const url = 'mongodb://localhost:27017' // connection URL
 const dbName = 'stadtfuehrerDB' // database name
-const collectionName = 'pois' // collection name
+const poisCollectionName = 'pois' // collection name
+const toursCollectionName = 'tours' // collection name
 //----------------------------------------------------------------------------------------------------------------------
 const MongoClient = require('mongodb').MongoClient //Client for MongoDB
 const client = new MongoClient(url) // mongodb client
@@ -28,6 +29,41 @@ router.get('/', function(req, res, next)
     {
       assert.strictEqual(err, null);
       res.json(docs); //return documents from Database
+    })
+  })
+});
+module.exports = router; //export as router
+
+
+
+
+///////////////////////////////////////////////////////////
+// get All Tours and their pois
+
+router.get('/getCollections', function(req, res, next) 
+{
+  //Connect to the mongodb database and retrieve all docs
+  client.connect(function(err) 
+  {
+    const db = client.db(dbName); //Database
+    const t_collection = db.collection(toursCollectionName); //tours collection
+    const p_collection = db.collection(poisCollectionName); //pois collection
+
+    // Find all documents
+    var t_result = []; //tour result
+    var p_result = []; //location result
+    t_collection.find({}).toArray(function(err, docs_1) 
+    {
+      t_result = docs_1; //store tours
+
+      p_collection.find({}).toArray(function(err, docs_2) 
+      {
+        p_result = docs_2; //store pois
+        var result = [];
+        result[0] = p_result;
+        result[1] = t_result; 
+        res.json(result); //return combines result
+      })
     })
   })
 });
